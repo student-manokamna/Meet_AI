f4941fc532ec6e5b969cc203ecaa272041845564
student-manokamna <aroramoney473@gmail.com>
2025-09-28 15:50:17 +0530

    chnages

--- src/modules/home/ui/view/home-view.tsx
+++ src/modules/home/ui/view/home-view.tsx
@@ -4,25 +4,17 @@ import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client"; 
 import { useState } from "react";
 import { useRouter  } from "next/navigation";
+import {useTRPC} from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query";
 
-
-
- export const HomeView =()=>{
-  const router= useRouter();
-  const {data:session} = authClient.useSession();
-  if(!session){
-    return(
-<p>Loading....</p>
-    )
-  }
-  return(
+export const HomeView =()=>{
+  const trpc = useTRPC();
+  const {data}=useQuery(trpc.hello.queryOptions({text:"Antonio"}))
+  return (
     <div className="flex flex-col p-4 gap-y-4">
-      <p>logged in as {session.user.name}</p>
-      <Button onClick={()=>{
-        authClient.signOut({fetchOptions:{onSuccess:()=>{
-          router.push("/");
-        }}});
-      }}>sign out</Button>
-    </div>  
+      {data?.greeting}
+      </div>
   )
 }
+
+
